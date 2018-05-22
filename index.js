c1e701706f6af20bc5ac471302f8c79267812106
Jonathan Kuhl <jonathankuhl@Jonathans-MBP.maine.rr.com>
2018-05-22 11:37:47 -0400

    updated json and code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const flatten = require('lodash.flatten');
 const has = require('lodash.has');
 
 function find(keyword, option) {
+    keyword = keyword.toLowerCase();
     let localDict = dict;
     let dictArray = [];
     Object.values(localDict).forEach( (value)=> {
@@ -14,10 +15,14 @@ function find(keyword, option) {
         return has(entry, keyword);
     });
     try {
+        console.log(`\n\n*****  ${dictArray[0][keyword].name.trim()}  *****\n\n`);
         console.log("Use:", dictArray[0][keyword].use);
         console.log("Example:", dictArray[0][keyword].example);
+        console.log("Parameters: ", dictArray[0][keyword].parameters);
+        console.log("Returns: ", dictArray[0][keyword].returns)
         console.log("Is on the following prototypes: ", dictArray[0][keyword].prototypes);
         console.log("More information: ", dictArray[0][keyword].mdn)
+        console.log(`\n\n**********\n\n`);
     } catch(error) {
         console.log(`The keyword ${keyword} was not found`);
     }
